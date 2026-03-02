edc0bbe0716df13f7586a72012a8a0b00efa6d8b
Arsen-Alex <arsen31082019@gmail.com>
2026-03-02 20:48:12 +0500

    MyFirst2_CI/CD

--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
-from main import add
+from main import add, add2
 
 def test_add():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
+
+def test_add2():
+    assert add2(2, 3) == 6
+    assert add2(0, 0) == 0
